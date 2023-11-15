2801c351abbdd7b295895304fd9c67e15231eb20
Othmaneamrani <othmaneamrani31@gmail.com>
2023-11-15 02:52:13 +0100

    codeFirst

---
+++ codeFirstNet/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace codeFirstNet.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
